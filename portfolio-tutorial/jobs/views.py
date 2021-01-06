d70f65d5f525bfc5ab6c706519f2eb452f7b6297
Fortuntato <37523381+Fortuntato@users.noreply.github.com>
2021-01-06 01:16:43 +0100

    Added second job and edit homepage to show job details
    Edit homepage html to show the data taken from the postgreDB

--- portfolio-tutorial/jobs/views.py
+++ portfolio-tutorial/jobs/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
-
+from .models import Job
 # Create your views here.
 def shouyi(request):
     return render(request, 'jobs/shouyi.html')
 
 def home(request):
-    return render(request, 'jobs/home.html')
+    jobs = Job.objects
+    return render(request, 'jobs/home.html', {'jobs':jobs})
+
