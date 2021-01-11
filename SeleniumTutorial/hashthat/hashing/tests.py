74b11e9c851acf27849126a32b322055f0394ceb
Fortuntato <37523381+Fortuntato@users.noreply.github.com>
2021-01-11 17:36:57 +0100

    Writing unit test for hashing page

--- SeleniumTutorial/hashthat/hashing/tests.py
+++ SeleniumTutorial/hashthat/hashing/tests.py
@@ -1,17 +1,33 @@
 from django.test import TestCase
 from selenium import webdriver
+from .forms import HashForm
 
-class FunctionalTestCase(TestCase):
+# class FunctionalTestCase(TestCase):
     
-    def setUp(self):
-        self.browser = webdriver.Firefox()
+#     def setUp(self):
+#         self.browser = webdriver.Firefox()
 
-    def test_thereIsHomepage(self):
-        self.browser.get('http://localhost:8000')
-        self.assertIn('install', self.browser.page_source)
+#     def test_thereIsHomepage(self):
+#         self.browser.get('http://localhost:8000')
+#         self.assertIn('Enter hash here', self.browser.page_source)
 
-    def tearDown(self):
-        self.browser.quit()
+#     def test_hashOfHello(self):
+#         self.browser.get('http://localhost:8000')
+#         text = self.browser.find_element_by_id('id_text')
+#         text.send_keys('hello')
+#         self.browser.find_element_by_name('submit').click()
+#         self.assertIn('2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824', self.browser.page_source)
+
+#     def tearDown(self):
+#         self.browser.quit()
 
 
+class UnitTestCase(TestCase):
+    
+    def test_homepage_template(self):
+        response = self.client.get('/') #give us the response for the homepage
+        self.assertTemplateUsed(response, 'hashing/home.html')
 
+    def test_hashForm(self):
+        form = HashForm(data={'text':'hello'})
+        self.assertTrue(form.is_valid())
