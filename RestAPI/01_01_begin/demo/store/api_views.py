779a98bc4c478add491c458be4aa27aa57da8549
Fortuntato <37523381+Fortuntato@users.noreply.github.com>
2021-01-14 18:11:06 +0100

    Adding filtering for products following the tutorial code

--- RestAPI/01_01_begin/demo/store/api_views.py
+++ RestAPI/01_01_begin/demo/store/api_views.py
@@ -1,4 +1,5 @@
 from rest_framework.generics import ListAPIView
+from django_filters.rest_framework import DjangoFilterBackend
 
 from store.serializers import ProductSerializer
 from store.models import Product
@@ -6,3 +7,19 @@ from store.models import Product
 class ProductList(ListAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    filter_backends = (DjangoFilterBackend,) #WHY THE BLOODY COMMA?
+    filter_fields = ('id',) #DANK FARRIK WHY THE BLOODY COMMA AT THE END??
+
+    def get_queryset(self):
+        on_sale = self.request.query_params.get('on_sale',None)
+        if on_sale is None:
+            return super().get_queryset()
+        queryset = Product.objects.all()
+        if on_sale.upper() == 'TRUE':
+            from django.utils import timezone
+            now = timezone.now()
+            return queryset.filter(
+                sale_start__lte=now,
+                sale_end__gte=now,
+            )
+        return queryset
